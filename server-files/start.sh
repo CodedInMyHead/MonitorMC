e33afdf74f62b3ce029531a70c0dbd087fee3aca
Micha S <empty>
2023-06-16 10:34:51 +0200

    add plugin and set docker network to bridge mode, actual prometheus config and connnect prometheus to mc. Also make filesystem in docker better

--- server-files/start.sh
+++ server-files/start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-SERVER_PATH="/minecraft/server-files"  # Path to server directory
+SERVER_PATH="/setup"  # Path to server directory
 EULA_FILE="$SERVER_PATH/eula.txt" # Location of expected eula file
 MIN_GB=1G # Min amount of GB to use for the Server
 MAX_GB=8G # Max amount of GB to use for the Server
 
-chmod -R a+r $SERVER_PATH # Change permissions to make files readable
-
+chmod -R a+rwx $SERVER_PATH # Change permissions to make files readable
 java -Xms"${MIN_GB}" -Xmx"${MAX_GB}" -jar "$SERVER_PATH/server.jar" nogui || # Start server to generate files
-chmod -R a+r $SERVER_PATH # Required for next step (idk why)
+chmod -R a+rwx $SERVER_PATH # Required for next step (idk why)
 echo "eula=true" > eula.txt # Change eula=false to true
-chmod -R a+r $SERVER_PATH # Make everything readable
+chmod -R a+rwx $SERVER_PATH # Make everything readable
 java -Xms"${MIN_GB}" -Xmx"${MAX_GB}" -jar "$SERVER_PATH/server.jar" nogui # Final start with all files there
